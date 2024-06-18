a95fc3815cbf6fddfeb1228f88628ae6a7ba35b9
chenjitong <liehuowutong@163.com>
2024-06-18 11:14:14 +0800

    优化display

--- strut-macro/src/display.rs
+++ strut-macro/src/display.rs
@@ -4,7 +4,7 @@ use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 use crate::{
-    util::field::{chk_named_st, chk_st, get_fields},
+    util::field::{chk_named_st, chk_st, get_fields, get_skip_fields},
     DISPLAY,
 };
 
@@ -42,16 +42,22 @@ pub(crate) fn display_impl(input: TokenStream) -> TokenStream {
     // 取所有有效成员变量
     let filter_fields = get_fields(fields, DISPLAY);
 
+    // 取所有有效成员变量
+    let skip_fields = get_skip_fields(fields, DISPLAY);
+
     // 结构体泛型信息：实现的 traits 的泛型参数，类型参数，泛型 where 子句限定
     let (impl_g, ty_g, where_c) = &drive_ast.generics.split_for_impl();
 
-    // 结构体成员模式识别
-    let refs = fields.named.iter().map(|f| {
+    // 结构体有效成员模式识别
+    let refs = filter_fields.iter().map(|f| {
         let fname = f.ident.clone().unwrap(); // 成员变量
-        match filter_fields.iter().any(|fx| fx.ident == f.ident) {
-            true => quote! {#fname,},
-            false => quote! {#fname : _,},
-        }
+        quote! {#fname,}
+    });
+
+    // 结构体略过成员模式识别
+    let refs_skiped = skip_fields.iter().map(|f| {
+        let fname = f.ident.clone().unwrap();
+        quote! {#fname : _,}
     });
 
     // display 的 formatter 对每个非 skip 成员变量的处理
@@ -65,7 +71,7 @@ pub(crate) fn display_impl(input: TokenStream) -> TokenStream {
 
     // 实现的 display 的 fmt 函数的函数体
     let display_fmt = quote! {
-        let #st_name{#(#refs)*} : &#st_name = self;
+        let #st_name{#(#refs)*#(#refs_skiped)*} : &#st_name = self;
         let name = ::core::any::type_name::<#st_name>();
         f.debug_struct(name)
             #(#fields_fmt)*
